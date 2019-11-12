37e4d0820b7daf6dcabd6b869a7500421c31423e
jae.L <jklee900414@gmail.com>
2019-11-12 16:06:45 +0000

    update about page

--- page-about.php
+++ page-about.php
@@ -15,13 +15,14 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+
 		<?php
 		// Start the loop.
 		while ( have_posts() ) :
 			the_post();
 
 			// Include the page content template.
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content', 'page-about' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
